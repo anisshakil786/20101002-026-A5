9bb51464066886ac3ff27f00ae148f970db03a87
Anis Shakeel <h.m.anisshakil786@gmail.com>
2024-01-18 19:59:03 +0500

    Add project files.

---
+++ Models/M_Product.cs
@@ -0,0 +1,39 @@
+namespace e_commerce.Models
+{
+    using System;
+    using System.Web;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class M_Product
+    {
+        [Key]
+        public int Product_Id { get; set; }
+
+        [StringLength(50)]
+        public string Product_Name { get; set; }
+
+        [StringLength(50)]
+        public string Product_Description { get; set; }
+
+        [StringLength(50)]
+        public string Product_Picture { get; set; }
+
+        [NotMapped]
+        public HttpPostedFileBase A_img { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal? Product_Sale_Price { get; set; }
+
+        [Column(TypeName = "numeric")]
+        public decimal? Product_Purchase_Price { get; set; }
+
+        public int? Category_FId { get; set; }
+
+        [NotMapped]
+        public int qty { get; set; }
+        public virtual M_Category M_Category { get; set; }
+    }
+}
